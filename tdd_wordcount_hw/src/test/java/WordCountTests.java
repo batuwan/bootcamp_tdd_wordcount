9abd9f1ee1f603390abb40bbdd101a297196b975
Batuhan Karaman <batuhankaraman@gmail.com>
2020-09-19 01:15:33 +0300

    Update WordCountTests.java

--- tdd_wordcount_hw/src/test/java/WordCountTests.java
+++ tdd_wordcount_hw/src/test/java/WordCountTests.java
@@ -1,24 +1,68 @@
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
 public class WordCountTests {
 
     @Test
-    public void getWordCount_WhenAStringIsGiven_ShouldReturnNumberOfWords() {
+    public void getWordCount_WhenAStringIsGiven_ShouldReturnTwo() {
 
         //Given
         WordCount sut = new WordCount();
         //When
         int count = sut.getWordCount("Batuhan Karaman");
         //Then
-        assertEquals(count,2);
+        assertEquals(count, 2);
 
-        /*
-         todo: number of unique words
-         fixme: capitalization
-        */
+
     }
 
+    @Test
+    public void getWordCount_WhenInputStringIsEmpty_ShouldThrowException() {
 
+        //Given
+        WordCount sut = new WordCount();
+        //When
+        Throwable thrown = Assertions.catchThrowable(() -> sut.getWordCount("!"));
+        //Then
+        assertEquals("There are no words here.", thrown.getMessage());
+
+    }
+
+    @Test
+    public void getWordCount_WhenInputHasDuplicateWords_ShouldReturnTwo() {
+
+        //Given
+        WordCount sut = new WordCount();
+        //When
+        int count = sut.getWordCount("Batuhan Karaman Batuhan");
+        //Then
+        assertEquals(count, 2);
+
+
+    }
+
+    @Test
+    public void getWordCount_WhenInputHasPunctuations_ShouldReturnThree() {
+
+        //Given
+        WordCount sut = new WordCount();
+        //When
+        int count = sut.getWordCount("Batuhan, Karaman!?\" Karaman?");
+        //Then
+        assertEquals(count, 3);
+
+
+    }
+
+    @Test
+    public void getWordCount_WhenInputHasBothUpperAndLowerCase_ShouldReturnTwo() {
+
+        //Given
+        WordCount sut = new WordCount();
+        //When
+        int count = sut.getWordCount("Batuhan batuhan Karaman karaman");
+        //Then
+        assertEquals(count, 2);
+
+
+    }
 }
