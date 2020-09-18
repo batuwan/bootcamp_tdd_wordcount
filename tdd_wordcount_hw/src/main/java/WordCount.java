fabc7405843bc5298874b9c8f258bdbef02306de
Batuhan Karaman <batuhankaraman@gmail.com>
2020-09-19 01:14:47 +0300

    Update WordCount.java

--- tdd_wordcount_hw/src/main/java/WordCount.java
+++ tdd_wordcount_hw/src/main/java/WordCount.java
@@ -1,10 +1,22 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class WordCount {
 
 
     public int getWordCount(String string) {
 
-        String [] arrOfStr = string.split("[;, ?.@!_|&%]");
-        int count = arrOfStr.length;
-        return count;
+        string = string.toLowerCase();
+        String[] arrOfStr = string.split("[;, \"'?.@!_|&%/\n\t]");
+        if (string.length() == 0 || arrOfStr.length == 0) {
+            throw new IllegalArgumentException("There are no words here.");
+        }
+        else {
+            Set<String> hashSet = new HashSet<>(Arrays.asList(arrOfStr));
+            return hashSet.size();
+
+        }
     }
 }
+
